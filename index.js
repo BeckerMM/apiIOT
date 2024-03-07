fabcea14353f5af1b17b6fdf2f71dd3229fb0410
Matheus Becker <beckerm@weg.net>
2024-03-07 19:00:49 -0300

    pronto

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const cors = require('cors');
-const { leiteenvasado, getLeiteenvasado, getDadosDoMiguel, postDadosDoMiguel } = require("./src/requisições/temperatura");
+const { leiteenvasado, getLeiteenvasado, getDadosDoMiguel, postDadosDoMiguel , getLuka} = require("./src/requisições/temperatura");
 
 app.use(cors({ origin: '*' }));
 app.use(express.json());
@@ -12,7 +12,10 @@ app.use(express.json());
 router.post(`/leiteenvasado`, (req, res) => leiteenvasado(req, res));
 router.post("/becker", (req, res) => postDadosDoMiguel(req, res));
 
+
+
 // Corrected callback functions for GET requests
+router.get("/luka", (req,res) => getLuka(res));
 router.get(`/leiteenvasado`, (req, res) => getLeiteenvasado(res));
 router.get("/becker", (req, res) => getDadosDoMiguel(res));
 
--- src/requisições/temperatura.js
+++ src/requisições/temperatura.js
@@ -1,7 +1,7 @@
 const leiteenvasadoobj = [];
 
 const dadosDoMiguel = []
-
+const dadosParaLuka = []
 
 
 // POST
@@ -39,9 +39,38 @@ function getDadosDoMiguel (res)  {
     return res.status(200).json({dadosDoMiguel});
 }
 
+function randomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+}
+
+const percorrerdados = (dado,array) => {
+    const obj = {};       
+    obj.dataValidade = randomDate(start, end).toLocaleDateString();
+    obj.quantidade = dados.quantidade;
+    obj.produto = dado.tipo
+    array.push(obj);
+}
+    
+
+function getLuka(res){
+    const start = new Date(2024, 1, 1); // Início do período
+    const end = new Date(start.getTime() + 1000 * 60 * 60 * 24 * 365 * 5); // Término do período
+
+    dadosDoMiguel.forEach((item) => {    
+        percorrerdados(item, dadosParaLuka);
+    })
+    leiteenvasadoobj.forEach((item) => {   
+        percorrerdados(item, dadosParaLuka);
+        
+    })
+    
+    return res.status(200).json({dadosParaLuka});
+                                                                                               
+}
 module.exports = {
     leiteenvasado,
     getLeiteenvasado, 
     postDadosDoMiguel,
-    getDadosDoMiguel
+    getDadosDoMiguel,
+    getLuka
 };
